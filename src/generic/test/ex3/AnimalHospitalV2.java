62e226132a04b95b9f1c0e451b62d7d138ad2200
이관영 <hicmcs123@gmail.com>
2026-02-05 08:00:10 +0900

    타입 매개변수 제한3 - 제네릭 도입과 실패

--- src/generic/test/ex3/AnimalHospitalV2.java
+++ src/generic/test/ex3/AnimalHospitalV2.java
@@ -1,4 +1,25 @@
 package generic.test.ex3;
 
-public class AnimalHospitalV2 {
+public class AnimalHospitalV2<T> {
+    private T animal;
+
+    public void set(T animal) {
+        this.animal = animal;
+    }
+
+    public void checkup() {
+        // T의 타입을 메서드를 정의하는 시점에는 알 수 없다. Object의 기능만 사용
+        animal.toString();
+        animal.equals(null);
+
+        //컴파일 오류
+//        System.out.println("동물 이름: "+ animal.getName());
+//        System.out.println("동물 크기: "+ animal.getSize());
+    }
+
+    public T getBigger(T target) {
+        //컴파일 오류
+//        return animal.getSize() >  target.getSize() ? target : animal;
+        return null;
+    }
 }
